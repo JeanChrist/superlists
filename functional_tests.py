572e63ab9b59685c748c6bdcca798a0fc98d7d92
Ji Chao <714316722@qq.com>
2018-12-15 14:08:57 +0800

    Functional test now can check we input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class TestCase(unittest.TestCase):
@@ -9,12 +11,38 @@ class TestCase(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_1(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:6789')
+
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        # input_content =
+        input_box.send_keys('Buy peacock feathers')
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
 
-    unittest.main(warnings='ignore')
+    unittest.main()
