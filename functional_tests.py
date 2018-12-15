317afc466f138b7c338046383e12434138fb08b3
Ji Chao <714316722@qq.com>
2018-12-15 15:00:41 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -28,14 +28,14 @@ class TestCase(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        # input_content =
         input_box.send_keys('Buy peacock feathers')
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
