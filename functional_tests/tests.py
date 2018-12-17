dc043f38f1ef3823aadbcf9ea41a22cf39b73753
Ji Chao <714316722@qq.com>
2018-12-17 17:13:27 +0800

    New wait decorator replace time.sleep in functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
 from django.test import LiveServerTestCase
+
+
+MAX_WAIT = 10
+
+
+def need_wait_decorator(func):
+    def wrap(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return wrap
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,7 +29,9 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @need_wait_decorator
     def check_for_row_in_list_table(self, row_text):
+
         table = self.browser.find_element_by_id('id_list_table')
 
         rows = table.find_elements_by_tag_name('tr')
@@ -37,7 +56,7 @@ class NewVisitorTest(LiveServerTestCase):
         # is tying fly-fishing lures)
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
@@ -46,7 +65,7 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
