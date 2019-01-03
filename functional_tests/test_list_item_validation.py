e1846250762905bb56fdd6ab0fcbb695ec775e10
Ji Chao <714316722@qq.com>
2019-01-03 17:25:52 +0800

    add jquery, qunit tests, list.js with keypress listeners

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,6 +6,9 @@ from lists.forms import EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -20,9 +23,10 @@ class ItemValidationTest(FunctionalTest):
         # self.have_error_text()
 
         # She tries again with some text for the item, which now works
-        self.get_item_input_box().send_keys('Buy milk')
+        text = 'Buy milk'
+        self.get_item_input_box().send_keys(text)
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table(f'1: {text}')
 
         # Perversely, she now decides to submit a second blank list item
         self.get_item_input_box().send_keys(Keys.ENTER)
@@ -34,7 +38,7 @@ class ItemValidationTest(FunctionalTest):
         # And she can correct it by filling some text in
         self.get_item_input_box().send_keys('Make tea')
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table(f'1: {text}')
         self.check_for_row_in_list_table('2: Make tea')
 
     def test_cannot_add_duplicate_items(self):
@@ -50,6 +54,26 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             DUPLICATE_ITEM_ERROR
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+
+        # Edith starts a list and causes a validation error:
+        self.browser.get(self.live_server_url)
+        text = 'Banter too thick'
+        self.get_item_input_box().send_keys(text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table(f'1: {text}')
+        self.get_item_input_box().send_keys(text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+        # She is pleased to see that the error message disappears
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
